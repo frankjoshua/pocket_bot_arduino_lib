0aa775e10af3f49339ee0a59a268c1247911080e
frankjoshua <frankjoshua@gmail.com>
2015-11-02 21:35:49 -0600

    Updated Protobuf

--- examples/pocketbot_usb/pocketbot_usb.ino
+++ examples/pocketbot_usb/pocketbot_usb.ino
@@ -1,11 +1,18 @@
-#include <Usb.h> //https://github.com/felis/USB_Host_Shield
-#include <AndroidAccessory.h> //https://github.com/bblanchon/ArduinoJson
-#include <PocketBot.h>
+#include <adk.h> //https://github.com/felis/USB_Host_Shield_2.0
+#include <usbhub.h> //https://github.com/felis/USB_Host_Shield_2.0
+#include <SPI.h>
+#include <PocketBot.h> //https://github.com/frankjoshua/PocketBot
+
+//Init USB connection for android device
+USB Usb;
+//These two lines give support for using a powered USB hub
+USBHub hub0(&Usb);
+USBHub hub1(&Usb);
 
 /*
 * These parameters must match what the app expects
 */
-AndroidAccessory acc("Tesseract Mobile LLC",
+ADK adk(&Usb, "Tesseract Mobile LLC",
 		     "PocketBot",
 		     "",
 		     "1.0",
@@ -20,15 +27,30 @@ PocketBotMessage message = PocketBotMessage_init_zero;
 void setup(void){
   Serial.begin(115200);
   
-  acc.powerOn();
+  //Connect to Android device
+  if(Usb.Init() == -1) {
+    Serial.println(F("OSCOKIRQ failed to assert"));
+    while(1); //halt
+  }
+  Serial.println(F("Waiting for Android device..."));
+  while(adk.isReady() == false){
+     Usb.Task();
+  }
+  Serial.println("");
+  Serial.println(F("Connected"));
 }
 
-byte in[1];
 void loop(void){
-
-  if(acc.isConnected()){ 
-    while(acc.read(in, sizeof(in), 1) > 0){
-      if(pocketBot.read(in[0], message)){
+  uint8_t rcode;
+  byte msg[1] = { 0x00 };
+  uint16_t len = sizeof(msg);
+  Usb.Task();
+  if(adk.isReady()){ 
+    len = sizeof(msg);
+    rcode = adk.RcvData(&len, msg);
+    while(!rcode && len > 0){
+      Usb.Task();
+      if(pocketBot.read(msg[0], message)){
         Serial.println(millis());
         Serial.print(F("FaceID = ")); Serial.println(message.face.faceId);
         Serial.print(F("FaceX = ")); Serial.println(message.face.faceX);
@@ -40,7 +62,7 @@ void loop(void){
         Serial.print(F("Proximity = ")); Serial.println(message.sensor.proximity);
         Serial.print(F("Heading = ")); Serial.println(message.sensor.heading);
       }
+      rcode = adk.RcvData(&len, msg);
     }
   }
 }
-
--- examples/pocketbot_usb_motor_example/pocketbot_usb_motor_example.ino
+++ examples/pocketbot_usb_motor_example/pocketbot_usb_motor_example.ino
@@ -1,16 +1,21 @@
-#include <Arduino.h>
-#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
-#include <Usb.h> //https://github.com/felis/USB_Host_Shield
-#include <AndroidAccessory.h>
-#include <PocketBot.h>
+#include <adk.h> //https://github.com/felis/USB_Host_Shield_2.0
+#include <usbhub.h> //https://github.com/felis/USB_Host_Shield_2.0
+#include <SPI.h>
+#include <PocketBot.h> //https://github.com/frankjoshua/PocketBot
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include <RunningMedian.h>
 
+//Init USB connection for android device
+USB Usb;
+//These two lines give support for using a powered USB hub
+USBHub hub0(&Usb);
+USBHub hub1(&Usb);
+
 /*
 * These parameters must match what the app expects
 */
-AndroidAccessory acc("Tesseract Mobile LLC",
+ADK adk(&Usb, "Tesseract Mobile LLC",
 		     "PocketBot",
 		     "",
 		     "1.0",
@@ -25,7 +30,10 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
 
 RunningMedian avg(5);
 
+/** This will be used to decode messages from the Android device */
 PocketBot pocketBot;
+/* Allocate space for the decoded message. */
+PocketBotMessage message = PocketBotMessage_init_zero;
 
 long mLastUpdate = 0;
 long mLastSensorRead = 0;
@@ -43,71 +51,64 @@ void setup(void){
   AFMS.begin();
   rightMotor->run(RELEASE);
   leftMotor->run(RELEASE);
-        
-  acc.powerOn();
-  pocketBot.begin();
   
-  Serial.println(F("Waiting for Android device"));
-  while(acc.isConnected() == false){
-    Serial.print(".");
-    delay(250); 
+ //Connect to Android device
+  if(Usb.Init() == -1) {
+    Serial.println(F("OSCOKIRQ failed to assert"));
+    while(1); //halt
   }
+  Serial.println(F("Waiting for Android device..."));
+  while(adk.isReady() == false){
+     Usb.Task();
+  }
+  Serial.println("");
+  Serial.println("Connected");
   
   // set pin for sonar
   pinMode(SonarPin, INPUT);
 }
 
 void loop(void){
-
-  if(acc.isConnected()){ 
-    byte in[1];
-    while(acc.read(in, sizeof(in), 1) > 0){
-      char input = (char) in[0];
-      Serial.print(input);
-      //pocketBot.printRawTo(Serial);
-      //Serial.println("");
-      if(pocketBot.read(input)){
-        JsonObject& root = pocketBot.getJson();
-        if(!root.success()){
-         Serial.println("");
-         Serial.println(F("*************** JSON ERROR **************")); 
-         return;
-        }
-        
-        //Serial.println("JSON");
-        for (JsonObject::iterator it=root.begin(); it!=root.end(); ++it){
-          Serial.print(it->key);
-          //Serial.print(" = ");
-          Serial.println(it->value.asString());
-        }
-        Serial.println("");
-        mProximityAlert = root[F("proximity")];
-        int faceId = root[F("face_id")];
+  uint8_t rcode;
+  byte msg[1] = { 0x00 };
+  uint16_t len = sizeof(msg);
+  Usb.Task();
+  if(adk.isReady()){ 
+    len = sizeof(msg);
+    rcode = adk.RcvData(&len, msg);
+    while(!rcode && len > 0){
+      Usb.Task();
+      if(pocketBot.read(msg[0], message)){
+     
+        mProximityAlert = message.sensor.proximity;
+        int faceId = message.face.faceId;
         if(faceId > 0){
           mRoamingMode = false;
           //Get face location
-          float x = root[F("face_x")];
-          float z = root[F("face_z")];
+          float x = message.face.faceX;
+          float z = message.face.faceZ;
           //Navigate towards face
           if(x > 1.1){
-            int s = mapfloat(x, 1, 2, 50, 100);
+            int s = mapfloat(x, 1, 2, 30, 70);
             Serial.println(s);
             rightMotor->run(FORWARD);
             rightMotor->setSpeed(s);
             leftMotor->run(BACKWARD);
             leftMotor->setSpeed(s);
           } else if(x < .9){
-            int s = mapfloat(x, 0.0, 1, 100, 50);
+            int s = mapfloat(x, 0.0, 1, 70, 30);
             Serial.println(s);
             rightMotor->run(BACKWARD);
             rightMotor->setSpeed(s);
             leftMotor->run(FORWARD);
             leftMotor->setSpeed(s);
-          } else if (z > .3 && z < .45){
+          } 
+          else if (z > .3 && z < .45){
             move(BACKWARD, 100);
           } else if ( z > .5) {
             move(FORWARD, 100);
-          }else {
+          }
+          else {
             rightMotor->setSpeed(0);
             leftMotor->setSpeed(0);
           }
@@ -116,6 +117,7 @@ void loop(void){
         }
         mLastUpdate = millis();
       }
+      rcode = adk.RcvData(&len, msg);
     }
   }
 
@@ -137,13 +139,13 @@ void loop(void){
       //If no obsticals move forward
       if(mDistance > 24){
         //Forward
-         move(BACKWARD, 255);
+         move(BACKWARD, 100);
       } else if(mDistance > 12){
         //slight turn  
         rightMotor->run(BACKWARD);
-        rightMotor->setSpeed(200);
+        rightMotor->setSpeed(75);
         leftMotor->run(BACKWARD);
-        leftMotor->setSpeed(255);     
+        leftMotor->setSpeed(100);     
       } else {
         //turn
         rightMotor->run(FORWARD);
@@ -168,4 +170,3 @@ void move(int dir, int speed){
   leftMotor->run(dir);
   leftMotor->setSpeed(speed);
 }
-
